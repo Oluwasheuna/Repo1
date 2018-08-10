9f15164060381e423387df5d8e7ed413857b1f2f
Oluwasheuna <42254507+Oluwasheuna@users.noreply.github.com>
2018-08-10 10:03:16 +0100

    Add files via upload

---
+++ Codelagos assignment.py
@@ -0,0 +1,13 @@
+a = int(input("Enter a\n"))
+b = int(input("Enter b\n"))
+c = int(input("Enter c\n"))
+X = (-b + 4 * a * c) / 2 * a
+print (X)
+
+
+
+Name = input('Enter your name:')
+age = eval(input('Enter your age here:'))
+c = 100-age
+u = 2018 + c
+print(Name,',', ' ', 'you will be hundred years old in', ' ', u, sep ='')
